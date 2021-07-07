0dc5181d5e0b5c7e6fbc09c92b4d553e936ba6ae
Sean Melody <seandmelody@gmail.com>
2021-07-06 17:25:43 -0700

    React router dom added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import Register from './Pages/Register';
+import Login from './Pages/Login';
+import Books from './Pages/Books';
 
 function App() {
   return (
     <div className="App">
       <h1>Reading List</h1>
-      <Register />
+      <Router>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+
+
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Books} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/Pages/Books.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Books = () => {
+    return (
+        <div>
+            <h1>Hi from home (Books)</h1>
+        </div>
+    )
+}
+
+export default Books
