13c10e355ed508e761eef90e905b18e83dff74e0
Sean Melody <seandmelody@gmail.com>
2021-07-10 11:01:44 -0700

    Search form styling changes

--- client/src/Components/SearchForm.js
+++ client/src/Components/SearchForm.js
@@ -7,30 +7,36 @@ const SearchForm = (props) => {
 
     //Return the search form!
     return (
-        <form className="border-dark">
-            {/* Header Component here */}
+        <>
 
-            <div className="row form-group">
-                <div className="col-md-9">
-                    {/* Input form for searching the GoogleBooksAPI */}
-                    <input
-                        onChange={props.handleInputChange}
-                        value={props.search}
-                        name="search"
-                        type="text"
-                        placeholder="Search For Books"
-                        className="form-control text-center border border-dark"
-                        id="search"
-                    />
+            <h3>Search</h3>
+            <form className="border-dark">
+                {/* Header Component here */}
+
+                <div className="row form-group justify-content-center">
+                    <div className="col-md-9">
+                        {/* Input form for searching the GoogleBooksAPI */}
+                        <input
+                            onChange={props.handleInputChange}
+                            value={props.search}
+                            name="search"
+                            type="text"
+                            placeholder="Search For Books"
+                            className="form-control text-center border border-dark"
+                            id="search"
+                        />
+                    </div>
                 </div>
-                {/* Button to start the search */}
-                <button
-                    onClick={props.handleFormSubmit} className="btn btn-outline-primary col-md-3">
-                    Search
+                <div className="row form-group justify-content-center">
+                    {/* Button to start the search */}
+                    <button
+                        onClick={props.handleFormSubmit} className="btn btn-outline-primary col-md-3">
+                        Search
                 </button>
 
-            </div>
-        </form>
+                </div>
+            </form>
+        </>
     )
 
 }
