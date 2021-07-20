e0a83a6906c433e150b6fa93958a96b26f108016
Sean Melody <seandmelody@gmail.com>
2021-07-20 13:27:08 -0700

    User Data set up on AppJs

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
+import { useEffect, useState } from "react"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import Register from './Pages/Register';
 import Login from './Pages/Login';
 import Confirm from './Pages/Confirm';
 import Books from './Pages/Books';
 import Search from './Pages/Search';
+import UserContext from './Context/UserContext'
+import axios from 'axios'
 
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    user: undefined,
+    token: undefined
+  })
+
+  const checkLoggedIn = async () => {
+    let token = localStorage.getItem("auth-token")
+
+    if (token === null) {
+      localStorage.setItem("auth-token", "")
+    }
+    else {
+      console.log("no auth token")
+    }
+
+  }
+
+  useEffect(() => {
+    checkLoggedIn()
+  })
+
+
   return (
     <div className="App">
       <h1>Reading List</h1>
@@ -19,14 +45,15 @@ function App() {
           <button className="btn btn-primary margin10">Login</button>
         </Link>
 
-
-        <Switch>
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/search" component={Search} />
-          <Route path="/confirm" component={Confirm} />
-          <Route path="/" component={Books} />
-        </Switch>
+        <UserContext.Provider value={{ userData, setUserData }} >
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/search" component={Search} />
+            <Route path="/confirm" component={Confirm} />
+            <Route path="/" component={Books} />
+          </Switch>
+        </UserContext.Provider>
       </Router>
     </div>
   );
