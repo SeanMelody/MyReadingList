0acb90366aa7891c63cd37fcbc0a6d5d652cd37d
Sean Melody <seandmelody@gmail.com>
2021-07-27 18:19:42 -0700

    Reading List console logging from database

--- client/src/Pages/Books.js
+++ client/src/Pages/Books.js
@@ -1,13 +1,48 @@
-import React, { useEffect } from 'react'
-
+import React, { useState, useEffect, useContext } from 'react'
+import { useHistory } from "react-router-dom"
+import userContext from "../Context/UserContext"
+import axios from "axios"
 const Books = (props) => {
 
+    const [readingList, setReadingList] = useState([])
 
-    const { savedBooks, getBooks } = props
+    const { userData } = useContext(userContext)
+
+    const history = useHistory()
 
     useEffect(() => {
-        // getBooks()
-        // eslint-disable-next-line
+        if (!userData.user) {
+            history.push("/login")
+        }
+
+        // console.log(userData.user)
+
+        userData.user
+            ? console.log(userData.user.displayName)
+            : console.log("User Loading")
+
+    }, [userData.user, history])
+
+
+    useEffect(() => {
+        const cancelToken = axios.CancelToken;
+        const source = cancelToken.source();
+
+        (async () => {
+            try {
+                const allBooks = await axios.get("/readingList", {
+                    cancelToken: source.token,
+                    headers: { "x-auth-token": localStorage.getItem("auth-token") },
+                })
+
+                console.log(allBooks.data)
+                setReadingList(allBooks.data)
+            } catch (err) {
+                console.log(err)
+            }
+
+        })()
+        return () => source.cancel()
     }, [])
 
     return (
