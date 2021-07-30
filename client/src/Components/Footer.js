ef4617756ef79559c8b3b58603ccb1e3aec4970c
Sean Melody <seandmelody@gmail.com>
2021-07-30 12:25:11 -0700

    Delete Account button functioning

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useHistory } from "react-router-dom"
+import userContext from "../Context/UserContext"
+import axios from 'axios'
 
 const Footer = (props) => {
 
-    const deleteAccount = () => {
+    // const { userData, setUserData } = useContext(userContext)
+
+    const history = useHistory()
+
+    const deleteAccount = async () => {
         console.log(props.user)
+        try {
+            await axios.delete("/users", {
+                headers: { "x-auth-token": localStorage.getItem("auth-token") },
+            });
+
+            history.push("/login");
+        } catch (err) {
+            console.log(`Error deleting: ${err}`)
+            console.log(err)
+        }
     }
 
+
+
+    // Use effect to make sure the user is signed in, and if not, then send to login
+    // useEffect(() => {
+    //     if (!userData.user) {
+    //         history.push("/login")
+    //     }
+
+    //     // console.log(userData.user)
+
+    //     userData.user
+    //         ? console.log(userData.user.displayName)
+    //         : console.log("User loading")
+
+    // }, [userData.user, history])
 
     return (
         <footer className="footer fixed-bottom">
