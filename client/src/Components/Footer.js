b58a454aba194be8451320c75419dcab6a4a49a7
Sean Melody <seandmelody@gmail.com>
2021-08-24 12:19:55 -0700

    Footer adjusted, delete account moved to books page

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -8,36 +8,13 @@ const Footer = (props) => {
     // Use history so we can send the user back to register if they delete account
     const history = useHistory()
 
-    // Delete account function
-    const deleteAccount = async () => {
-        // console.log(props.user)
-        // Try catch to send request to Database to delete user and their books
-
-        // confirm("Are you sure you want to delete your account?");
-
-
-        try {
-            await axios.delete("/users", {
-                headers: { "x-auth-token": localStorage.getItem("auth-token") },
-            });
-            // Push the user back to register
-            history.push("/login");
-        }
-        // Gotta catch them errors!
-        catch (err) {
-            console.log(`Error deleting: ${err}`)
-            console.log(err)
-        }
-    }
-
     // Return the Footer Component
     return (
         // Footer fixed to the bottom of the page
-        <footer className="footer fixed-bottom">
+        <footer className="footer">
             {/* copyright and love 2021 */}
             <h5 className="margin10">&#169; and &#9829; Sean Melody 2021</h5>
-            {/* On click button to delete account */}
-            <button onClick={deleteAccount} className="btn btn-danger margin10">Delete Account</button>
+
         </footer>
     )
 }
--- client/src/Pages/Books.js
+++ client/src/Pages/Books.js
@@ -40,6 +40,28 @@ const Books = () => {
         // window.location.reload()
     }
 
+    // Delete account function
+    const deleteAccount = async () => {
+        // console.log(props.user)
+        // Try catch to send request to Database to delete user and their books
+
+        // confirm("Are you sure you want to delete your account?");
+
+
+        try {
+            await axios.delete("/users", {
+                headers: { "x-auth-token": localStorage.getItem("auth-token") },
+            });
+            // Push the user back to register
+            history.push("/login");
+        }
+        // Gotta catch them errors!
+        catch (err) {
+            console.log(`Error deleting: ${err}`)
+            console.log(err)
+        }
+    }
+
     useEffect(() => {
         if (!userData.user) {
             history.push("/welcome")
@@ -76,50 +98,54 @@ const Books = () => {
     }, [])
 
     return (
-        <div>
-            <h1 className="margin10">Your Reading List</h1>
+        <>
+            <div>
+                <h1 className="margin10">Your Reading List</h1>
 
-            {readingList.length ? (
-                <div className="container">
-                    {/* Map through the results from the Mongoose Database */}
-                    {readingList.map((book) => (
-                        // Set each book to a card
-                        <div className="card" key={book._id}>
-                            <div className="card-title row justify-content-around" >
-                                <h4 className="col-md-5">{book.title}</h4>
-                                {/* Button to view the book on google books */}
-                                <button
-                                    className="btn btn-outline-primary margin10">
-                                    <a
-                                        href={book.link} target="_blank" rel="noopener noreferrer"
-                                    >
-                                        View on Google Books
+                {readingList.length ? (
+                    <div className="container">
+                        {/* Map through the results from the Mongoose Database */}
+                        {readingList.map((book) => (
+                            // Set each book to a card
+                            <div className="card" key={book._id}>
+                                <div className="card-title row justify-content-around" >
+                                    <h4 className="col-md-5">{book.title}</h4>
+                                    {/* Button to view the book on google books */}
+                                    <button
+                                        className="btn btn-outline-primary margin10">
+                                        <a
+                                            href={book.link} target="_blank" rel="noopener noreferrer"
+                                        >
+                                            View on Google Books
                                             </a>
-                                </button>
-                                {/* Button to call the delete book function */}
-                                <button
-                                    className="btn btn-outline-danger margin10"
-                                    onClick={() => deleteBook(book)}>Delete Book
+                                    </button>
+                                    {/* Button to call the delete book function */}
+                                    <button
+                                        className="btn btn-outline-danger margin10"
+                                        onClick={() => deleteBook(book)}>Delete Book
                                         </button>
 
+                                </div>
+                                {/* Div to dispaly the card body, image, authors and descrioption */}
+                                <div className="card-body row">
+                                    <img className="col-md-2" src={book.image} alt="book cover" />
+                                    <h3 className="col-md-2">{book.authors}</h3>
+                                    <p className="col-md-8">{book.description}</p>
+                                </div>
                             </div>
-                            {/* Div to dispaly the card body, image, authors and descrioption */}
-                            <div className="card-body row">
-                                <img className="col-md-2" src={book.image} alt="book cover" />
-                                <h3 className="col-md-2">{book.authors}</h3>
-                                <p className="col-md-8">{book.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                    {/* A break for some space */}
-                    <br></br>
-                </div>
-            ) : (
-                <h1>No saved books</h1>
+                        ))}
+                        {/* A break for some space */}
+                        <br></br>
+                    </div>
+                ) : (
+                    <h1>No saved books</h1>
 
-            )
-            }
-        </div>
+                )
+                }
+            </div>
+            {/* On click button to delete account */}
+            <button onClick={deleteAccount} className="btn btn-danger margin10">Delete Account</button>
+        </>
     )
 }
 
