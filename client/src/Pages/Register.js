839e357aa10b25ebfac71c4f7ebbdb2a01d1b163
Sean Melody <seandmelody@gmail.com>
2022-02-02 17:08:55 -0800

    Register Page style changed to fit new login page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -30,18 +30,25 @@ const Register = () => {
     return (
         <div>
             <h1>Register</h1>
-            <div className="container">
+            <div className="container text-center justify-content-center space-large">
                 <form onSubmit={submit}>
-                    <h3>Email</h3>
-                    <input onChange={onChange} type="text" name="email" className="row col-md-12 text-center"></input>
-                    <h3>User Name</h3>
-                    <input onChange={onChange} type="text" name="displayName" className="row col-md-12 text-center"></input>
-                    <h3>Password</h3>
-                    <input onChange={onChange} type="text" name="password" className="row col-md-12 text-center"></input>
-                    <h3>Please Re-type your password</h3>
-                    <input onChange={onChange} type="text" name="passwordCheck" className="row col-md-12 text-center"></input>
-
-                    <input type="submit" className="btn btn-primary margin10" />
+                    <h3 className="col-md-12 marginTop">Email</h3>
+                    <div className="row justify-content-center">
+                        <input onChange={onChange} type="text" name="email" className="row col-md-6 text-center input-space"></input>
+                    </div>
+                    <h3 className="col-md-12 marginTop">User Name</h3>
+                    <div className="row justify-content-center">
+                        <input onChange={onChange} type="text" name="displayName" className="row col-md-6 text-center input-space"></input>
+                    </div>
+                    <h3 className="col-md-12 marginTop">Password</h3>
+                    <div className="row justify-content-center">
+                        <input onChange={onChange} type="text" name="password" className="row col-md-6 text-center input-space"></input>
+                    </div>
+                    <h3 className="col-md-12 marginTop">Please Re-type your password</h3>
+                    <div className="row justify-content-center">
+                        <input onChange={onChange} type="text" name="passwordCheck" className="row col-md-6 text-center input-space"></input>
+                    </div>
+                    <input type="submit" className="btn btn-primary button-spacing border-dark" />
                 </form>
             </div>
         </div>
